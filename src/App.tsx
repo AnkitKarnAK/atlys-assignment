6e0eb4a1beb4e999b8e50e8b93e51bdc228eb1b4
ankitkarnak <ankit.karn.ak@gmail.com>
2024-08-09 01:08:19 +0530

    style: Update App component styling to use dark text color

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
+    <div>
+      <h1 className="text-3xl font-bold underline text-dark-500">
         Hello world!
       </h1>
-    </>
+    </div>
   )
 }
 
