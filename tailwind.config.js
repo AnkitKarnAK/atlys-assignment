9dec8722c31804ef71f6da82ac6d5efd4599e4d6
ankitkarnak <ankit.karn.ak@gmail.com>
2024-08-09 01:08:09 +0530

    feat: Add custom colors to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: "#131319", // page background
+          300: "#191920", // post content background
+        },
+        accent: {
+          500: "#4A96FF", // cta button
+        },
+        dark: {
+          black: "#000000", // post card title
+          500: "#27292D", // post card background
+          300: "#35373B", // post card stroke
+        },
+        light: {
+          500: "#C5C7CA", // post card title
+          300: "#7F8084", // post card text
+          white: "#FFFFFF", // post card text
+        },
+      },
+    },
   },
   plugins: [],
 };
