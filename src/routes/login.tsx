2d1e747037628ea424ffdf357dd348a127f12329
ankitkarnak <ankit.karn.ak@gmail.com>
2024-08-12 01:28:18 +0530

    style: Update login and register routes to use main element for better semantic structure

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -3,9 +3,9 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/login')({
     component: () => (
-        <div className='h-dvh flex flex-col justify-center items-center'>
+        <main className="h-screen m-auto max-w-[700px] mobile:p-5 flex flex-col justify-center items-center">
             <SvgIcons icon={"Logo"} color='#C5C7CA' width={40} className='mb-12' />
             <LoginDialog />
-        </div>
+        </main>
     )
 })
--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -3,10 +3,10 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/register')({
     component: () => (
-        <div className='h-dvh flex flex-col justify-center items-center'>
+        <main className="h-screen m-auto max-w-[700px] mobile:p-5 flex flex-col justify-center items-center">
             <SvgIcons icon={"Logo"} color='#C5C7CA' width={40} className='mb-12' />
 
             <RegisterDialog />
-        </div>
+        </main>
     )
 })
