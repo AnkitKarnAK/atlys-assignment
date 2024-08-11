c2f60689d47e52049acbc359265cb5c2a7ea56dc
ankitkarnak <ankit.karn.ak@gmail.com>
2024-08-12 01:14:00 +0530

    style: Update logo color to use currentColor for dynamic styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className='h-dvh flex flex-col justify-center items-center'>
-      <SvgIcons icon={"Logo"} width={40} className='mb-12' />
+      <SvgIcons icon={"Logo"} color='#C5C7CA'  width={40} className='mb-12' />
       <LoginDialog />
     </div>
   )
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -4,7 +4,7 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/login')({
     component: () => (
         <div className='h-dvh flex flex-col justify-center items-center'>
-            <SvgIcons icon={"Logo"} width={40} className='mb-12' />
+            <SvgIcons icon={"Logo"} color='#C5C7CA' width={40} className='mb-12' />
             <LoginDialog />
         </div>
     )
