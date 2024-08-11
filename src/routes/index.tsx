4d6135b2e0f09007917fe53cb7b828d45b1a8849
ankitkarnak <ankit.karn.ak@gmail.com>
2024-08-12 01:28:26 +0530

    style: Update landing page with greeting message and introduction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,14 @@ export const Route = createFileRoute('/')({
 function Index() {
     return (
         <main className="m-auto max-w-[700px] py-[70px] mobile:p-5">
+
+            <div className={"w-full max-w-[580px] mb-10"}>
+                <h1 className="mb-3 text-xl font-medium leading-none text-light-500">Hello Jane</h1>
+                <p className="text-base font-normal text-light-300">
+                    How are you doing today? Would you like to share something with the community 🤗
+                </p>
+            </div>
+
             <FeedCard />
         </main>
     )
