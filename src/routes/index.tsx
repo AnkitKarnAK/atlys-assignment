b9279f80345f0d6c246188c40c0b1f536a365542
ankitkarnak <ankit.karn.ak@gmail.com>
2024-08-12 04:02:16 +0530

    refactor: Update landing page to display personalized greeting and introduction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { AddFeed, FeedCard } from '@/components'
+import { useAuthUser, useIsLoggedIn } from '@/store/auth-store'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -6,19 +7,31 @@ export const Route = createFileRoute('/')({
 })
 
 function Index() {
+
+    const authUser = useAuthUser();
+    const isLoggedIn = useIsLoggedIn();
+
+
+
     return (
-        <main className="m-auto max-w-[700px] py-[70px] mobile:p-5">
+        <>
+            <main className="m-auto max-w-[700px] py-[70px] mobile:p-5">
 
-            <div className={"w-full max-w-[580px] mb-10"}>
-                <h1 className="mb-3 text-xl font-medium leading-none text-light-500">Hello Jane</h1>
-                <p className="text-base font-normal text-light-300">
-                    How are you doing today? Would you like to share something with the community 🤗
-                </p>
-            </div>
+                {isLoggedIn && <div className="absolute top-0 right-2 p-5 text-xs text-light-500">
+                    <button className="underline">Logout</button>
+                </div>}
 
-            <AddFeed />
+                <div className={"w-full max-w-[580px] mb-10"}>
+                    <h1 className="mb-3 text-xl font-medium leading-none text-light-500">Hello {authUser?.name || authUser?.username || "User"}</h1>
+                    <p className="text-base font-normal text-light-300">
+                        How are you doing today? Would you like to share something with the community 🤗
+                    </p>
+                </div>
 
-            <FeedCard />
-        </main>
+                <AddFeed />
+
+                <FeedCard />
+            </main>
+        </>
     )
 }
